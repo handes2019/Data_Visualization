fe549f02cbaccdbe45b2cb85d13318499ace8356
handes <2451322819@qq.com>
2020-02-12 18:19:23 +0800

    2020/2/12

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+
+# 1、
+
+# plt.plot(squares)
+
+#2、
+
 plt.plot(squares, linewidth=5)
 
 #设置图表标题，并给坐标轴加上标签
@@ -11,5 +18,5 @@ plt.ylabel("Square of Value", fontsize=14)
 #设置刻度标记的大小
 plt.tick_params(axis='both', labelsize=14)
 
-# plt.plot(squares)
+
 plt.show()
