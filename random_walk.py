be9a1f2cdd25b3603c842d81399f391dd41f9d35
handes <2451322819@qq.com>
2020-02-13 17:42:42 +0800

    2020/2/12

--- random_walk.py
+++ random_walk.py
@@ -3,7 +3,7 @@ from random import choice
 class RandomWalk():
     """一个生成随机漫步数据的类"""
 
-    def __init__(self, num_points=5000):
+    def fill_walk(self, num_points=5000):
         """初始化随机漫步的属性"""
         #不断漫步，直到列表达到指定的长度
         while len(self.x_values) < self.num_points:
