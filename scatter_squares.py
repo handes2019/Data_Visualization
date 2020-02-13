c80812a027bb7c4ac3cb67a28c1f8c36e6882a71
handes <2451322819@qq.com>
2020-02-13 17:05:10 +0800

    2020/2/12

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 #plt.scatter(2, 4)
 
 #2、
-plt.scatter(2, 4, s=200)
+# plt.scatter(2, 4, s=200)
+
+#3、
+
+x_values=[1, 2, 3, 4, 5]
+y_values=[1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=100)
+
+
 #设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
