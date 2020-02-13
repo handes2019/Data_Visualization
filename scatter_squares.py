3b50621a260b72db54069a195426cf1e88338a9f
handes <2451322819@qq.com>
2020-02-13 17:11:27 +0800

    2020/2/12

--- scatter_squares.py
+++ scatter_squares.py
@@ -24,7 +24,7 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=24)
 
 #设置刻度标记的大小
-plt.tick_params(axis='both', which='major', labelsize=14)
+# plt.tick_params(axis='both', which='major', labelsize=14)
 
 #设置每个坐标轴的取值范围
 plt.axis([0, 1100, 0, 1100000])
