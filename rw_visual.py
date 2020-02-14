a6d70f66d4baf10815779f064b829cf4f3270ade
handes <2451322819@qq.com>
2020-02-14 16:26:54 +0800

    2020/2/12

--- rw_visual.py
+++ rw_visual.py
@@ -17,6 +17,9 @@ while True:
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 
+    #突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     plt.show()
 
