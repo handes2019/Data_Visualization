b7af194c5fbe3bbf4aed994827574fa91a320cdb
handes <2451322819@qq.com>
2020-02-14 16:22:23 +0800

    2020/2/12

--- rw_visual.py
+++ rw_visual.py
@@ -9,7 +9,15 @@ while True:
     #创建一个RandomWalk实例，并将其包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    #1、
+    #plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    #2、
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+
     plt.show()
 
     keep_running = input("Make anothr walk?(y/n)")
